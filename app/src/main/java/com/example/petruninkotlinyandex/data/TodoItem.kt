4e6a43058929760f850d0e0a7dd19aa822ee8dcd
Kaz1deX <max2003255@gmail.com>
2023-06-16 23:45:50 +0300

    Add new tasks

--- app/src/main/java/com/example/petruninkotlinyandex/data/TodoItem.kt
+++ app/src/main/java/com/example/petruninkotlinyandex/data/TodoItem.kt
@@ -2,11 +2,9 @@ package com.example.petruninkotlinyandex.data
 
 // Модель элемента задачи
 data class TodoItem(
-    var checkBoxTaskText: String,    // Текст задачи для отображения в чекбоксе
-    var importance: String,          // Важность задачи
-    var deadlineDate: String = ""    // Дата крайнего срока выполнения задачи
-) {
-    var idTask: String = "0"         // Идентификатор задачи
-    var isCompleted: Boolean = false // Флаг, указывающий, завершена ли задача
-    var currentDate: String = ""     // Дата создания задачи
-}
+    var checkBoxTaskText: String,     // Текст задачи для отображения в чекбоксе
+    var importance: String,           // Важность задачи
+    var deadlineDate: String = "",    // Дата крайнего срока выполнения задачи
+    var idTask: String = "0",         // Идентификатор задачи
+    var isCompleted: Boolean = false, // Флаг, указывающий, завершена ли задача
+    var currentDate: String = "") {}  // Дата создания задачи
